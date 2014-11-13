e8dc844887fb7bec3f897e3dd6d6da353bee9d84
achalbi <achal.rvce@gmail.com>
2014-11-13 19:57:48 +0530

    puma set

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,3 @@
-=begin
 workers Integer(ENV['PUMA_WORKERS'] || 3)
 threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
@@ -8,6 +7,7 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+=begin
 on_worker_boot do
   # worker specific setup
  # ActiveSupport.on_load(:active_record) do
